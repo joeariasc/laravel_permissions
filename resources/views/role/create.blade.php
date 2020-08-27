af426674c1efb35aa6a9084e9bd00c0da35cfc68
JArias <jarias@wawand.co>
2020-08-27 10:16:29 -0500

    Add the validation to keep the data after to process the form

--- resources/views/role/create.blade.php
+++ resources/views/role/create.blade.php
@@ -19,6 +19,7 @@
                                     id="name" 
                                     placeholder="Name"
                                     name="name"
+                                    value="{{ old('name')}}"
                                 >
                             </div>
                             <div class="form-group">                            
@@ -27,19 +28,49 @@
                                     id="slug" 
                                     placeholder="Slug"
                                     name="slug"
+                                    value="{{ old('slug')}}"
                                 >
                             </div>
                             <div class="form-group">                   
-                                <textarea class="form-control" placeholder="Description" name="description" id="description" rows="3"></textarea>
+                                <textarea
+                                    class="form-control"
+                                    placeholder="Description"
+                                    name="description"
+                                    id="description"
+                                    rows="3"
+                                >
+                                {{ old('description')}}
+                                </textarea>
                             </div>    
                             <hr>
                             <h3>{{ __('Full Access') }}</h3>
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" id="full_access_yes" name="full_access" class="custom-control-input" value="yes">
+                                <input 
+                                    type="radio" 
+                                    id="full_access_yes" 
+                                    name="full_access" 
+                                    class="custom-control-input" 
+                                    value="yes"
+                                    @if (old('full_access') == "yes") 
+                                        checked 
+                                    @endif
+                                >
                                 <label class="custom-control-label" for="full_access_yes">{{ __('Yes') }}</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" id="full_access_no" name="full_access" class="custom-control-input" value="no" checked>
+                                <input 
+                                    type="radio" 
+                                    id="full_access_no" 
+                                    name="full_access" 
+                                    class="custom-control-input" 
+                                    value="no"
+                                    @if (old('full_access') == "no") 
+                                    checked 
+                                    @endif
+                                    @if (old('full_access')===null) 
+                                    checked 
+                                    @endif
+                                >
                                 <label class="custom-control-label" for="full_access_no">{{ __('NO') }}</label>
                             </div>
                             <hr>
@@ -47,11 +78,15 @@
                             <h3>{{ __('Permission List') }}</h3>
                                 @foreach($permissions as $permission)                              
                                 <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" 
-                                    class="custom-control-input" 
-                                    id="permission_{{$permission->id}}"
-                                    value="{{$permission->id}}"
-                                    name="permission[]"
+                                    <input 
+                                        type="checkbox" 
+                                        class="custom-control-input" 
+                                        id="permission_{{$permission->id}}"
+                                        value="{{$permission->id}}"
+                                        name="permission[]"
+                                        @if( is_array(old('permission')) && in_array("$permission->id", old('permission'))    )
+                                        checked
+                                        @endif
                                     >
                                     <label class="custom-control-label" 
                                         for="permission_{{$permission->id}}">
