961e30f22312f32e8a4e73f3621607c9a64ec7a8
JArias <jarias@wawand.co>
2020-08-27 20:19:24 -0500

    adding the view and the method to edit a role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -44,7 +44,7 @@ class RoleController extends Controller
             'full_access' => 'required|in:yes,no'
         ]);
         $role = Role::create($request->all());
-        
+
         if ($request->get('permission')) {
             $role->permissions()->sync($request->get('permission'));
         }
@@ -69,9 +69,14 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Role $role)
     {
-        //
+        $permission_role = [];
+        foreach ($role->permissions as $permission) {
+            $permission_role[] = $permission->id;
+        }
+        $permissions = Permission::get();
+        return view('role.edit', compact('permissions', 'role', 'permission_role'));
     }
 
     /**
@@ -81,9 +86,23 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        //dd($request->all(), $role);
+        $request->validate([
+            'name'          => 'required|max:50|unique:roles,name,' . $role->id,
+            'slug'          => 'required|max:50|unique:roles,slug,' . $role->id,
+            'full_access'   => 'required|in:yes,no'
+        ]);
+
+        $role->update($request->all());
+
+        if ($request->get('permission')) {
+            //return $request->all();
+            $role->permissions()->sync($request->get('permission'));
+        }
+        return redirect()->route('role.index')
+            ->with('status_success', __('Role updated successfully'));
     }
 
     /**
